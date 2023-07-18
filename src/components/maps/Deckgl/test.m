a07228087a61532e874c139b5b79bf77ee391d35
Sebastian Meier <contact@sebastianmeier.eu>
2023-07-18 16:41:33 +0200

    links for ref

---
+++ src/components/maps/Deckgl/test.m
@@ -0,0 +1,2 @@
+https://dev.to/samuelearl/building-a-geospacial-app-with-sveltekit-deckgl-and-mapbox-part-2-scatterplot-overlay-1cdp 
+https://github.com/arperyan/svelte-deckgl/blob/main/src/components/Deckgl.svelte 
